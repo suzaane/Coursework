c01327dbeea1a487ac336d67471b2d41a78438c1
suzaane <suzanynshah@gmail.com>
2026-05-08 23:45:36 +0545

    Modified the files again

--- messaging/context_processors.py
+++ messaging/context_processors.py
@@ -1,11 +1,6 @@
 from .models import Message
 
-
 def unread_message_count(request):
-    """
-    Makes 'unread_count' available in ALL templates automatically.
-    This powers the red badge on the Messages sidebar link.
-    """
     if request.user.is_authenticated:
         count = Message.objects.filter(
             recipient=request.user,
@@ -14,5 +9,5 @@ def unread_message_count(request):
             deleted_by_recipient=False
         ).count()
         return {'unread_count': count}
+    # If the user is not authenticated, return 0 directly – do NOT use count here
     return {'unread_count': 0}
-
