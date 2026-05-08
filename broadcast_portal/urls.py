51564f99b605048772c41dd1e436f0ef180deee6
Student2 <kafinaprajapai21@gmail.com>
2026-05-08 20:50:30 +0545

    Fix custom admin URLs for statistics and reports

--- broadcast_portal/urls.py
+++ broadcast_portal/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from organisation.admin_dashboard import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path('organisation/', include('organisation.urls')),
 ]
