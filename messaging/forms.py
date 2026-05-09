b84d0d20796a8daeb925b87b961d9303f0dd75e2
suzaane <suzanynshah@gmail.com>
2026-05-09 13:29:11 +0545

    solved the forms error

--- messaging/forms.py
+++ messaging/forms.py
@@ -1,21 +1,23 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model          # ← CORRECT
 from .models import Message
+
+User = get_user_model()                                 # ← add this line
 
 
 class ComposeMessageForm(forms.ModelForm):
 
     recipient = forms.ModelChoiceField(
-    queryset=User.objects.all(),
-    required=False,
-    empty_label="Select Recipient",
-    widget=forms.Select(
-        attrs={
-            'class': 'form-select form-control'
-        }
-    ),
-    help_text="Leave empty to save as draft."
-)
+        queryset=User.objects.all(),
+        required=False,
+        empty_label="Select Recipient",
+        widget=forms.Select(
+            attrs={
+                'class': 'form-select form-control'
+            }
+        ),
+        help_text="Leave empty to save as draft."
+    )
     subject = forms.CharField(
         max_length=255,
         widget=forms.TextInput(
